87db64eefa976dc0878cba75b06f3c850621ec95
elmahask <welmahask@gmail.com>
2021-05-02 19:51:06 +0200

    final push today for methods

---
+++ src/javacollections/JavaCollections.java
@@ -0,0 +1,16 @@
+package javacollections;
+
+/**
+ *
+ * @author Wael M Elmahask
+ */
+public class JavaCollections {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
